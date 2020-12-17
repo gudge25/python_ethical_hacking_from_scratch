7a56d2a50c4e5cb9aa79f3ae87554e87ddd063a2
Antonio Italiano <satskiy.a@gmail.com>
2020-12-17 23:26:19 +0200

    lesson 4.12 Improve using list if Dictionaries

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -8,12 +8,18 @@ def scan(ip):
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+
+
     print("-----------------------------------------------------")
     print("|   IP\t\t\t MAC Address   |\n-----------------------------------------------------")
     #print(answered_list.summary())
+    client_list = []
     for element in answered_list:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc }
+        client_list.append(client_dict)
         print(element[1].psrc + "\t\t" + element[1].hwsrc)
         #print(element[1].show())
-
+    print(client_list)
 # scan("192.168.100.1")
 scan("192.168.100.1/24")
