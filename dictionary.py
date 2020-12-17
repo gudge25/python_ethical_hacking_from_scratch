e9e88a5572533f13e11b17b374e8cd4e384f6548
Antonio Italiano <satskiy.a@gmail.com>
2020-12-17 23:15:52 +0200

    lesson 4.11 dictionary example

---
+++ dictionary.py
@@ -0,0 +1,4 @@
+target_client = {"mac": "00:11:22:33:44:55", "ip": "192.168.156.5", "os": "windows"}
+
+print(target_client)
+print(target_client["os"])
--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -8,11 +8,12 @@ def scan(ip):
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-    print("IP\t\t\tMAC Address\n-----------------------------------------------------")
-    #print(answered.summary())
+    print("-----------------------------------------------------")
+    print("|   IP\t\t\t MAC Address   |\n-----------------------------------------------------")
+    #print(answered_list.summary())
     for element in answered_list:
         print(element[1].psrc + "\t\t" + element[1].hwsrc)
-
+        #print(element[1].show())
 
 # scan("192.168.100.1")
 scan("192.168.100.1/24")
