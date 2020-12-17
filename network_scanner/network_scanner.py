5b3078a7933d9e557b7763b12f647634decb8ba4
Antonio Italiano <satskiy.a@gmail.com>
2020-12-17 23:39:07 +0200

    lesson 4.13 Iterating data

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -9,17 +9,19 @@ def scan(ip):
     arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
-
-
-    print("-----------------------------------------------------")
-    print("|   IP\t\t\t MAC Address   |\n-----------------------------------------------------")
     #print(answered_list.summary())
     client_list = []
     for element in answered_list:
         client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc }
         client_list.append(client_dict)
-        print(element[1].psrc + "\t\t" + element[1].hwsrc)
         #print(element[1].show())
-    print(client_list)
-# scan("192.168.100.1")
-scan("192.168.100.1/24")
+    return client_list
+
+def print_result(results_list):
+    print("-----------------------------------------------------")
+    print("|   IP\t\t\t MAC Address   |\n-----------------------------------------------------")
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+scan_sesult = scan("192.168.100.1/24")
+print_result(scan_sesult)
