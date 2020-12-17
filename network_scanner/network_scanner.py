bd188b031937dc93fb344e51384ad99a03a49759
Antonio Italiano <satskiy.a@gmail.com>
2020-12-17 22:03:24 +0200

    lesson 4.7

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -5,18 +5,19 @@ from scapy.layers.l2 import arping
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
-    arp_request.show()
+    #arp_request.show()
     # arp_request.pdst=ip
     # arping(ip)
     # print(arp_request.summary())
     # scapy.ls(scapy.ARP())
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     # scapy.ls(scapy.Ether())
-    broadcast.show()
-    arp_request_broadcast = broadcast / arp_request
+    #broadcast.show()
+    arp_request_broadcast = broadcast/arp_request
     # print(arp_request_broadcast.summary())
-    arp_request_broadcast.show()
-
+    #arp_request_broadcast.show()
+    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
+    print(answered.summary())
 
 # scan("192.168.100.1")
 scan("192.168.100.1/24")
